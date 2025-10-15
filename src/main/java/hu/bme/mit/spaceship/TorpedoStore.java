ca01396b1a2ef33e8c06ea36a54ea1f99d46b39d
unknown <190667@bme.hu>
2025-10-15 14:41:13 +0200

    fix random generator

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -11,10 +11,11 @@ public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
-
+  Random generator;
   private int torpedoCount = 0;
 
   public TorpedoStore(int numberOfTorpedos){
+    generator = new Random();
     this.torpedoCount = numberOfTorpedos;
 
     // update failure rate if it was specified in an environment variable
@@ -36,7 +37,6 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    Random generator = new Random();
     double r = generator.nextDouble();
 
     if (r >= FAILURE_RATE) {
